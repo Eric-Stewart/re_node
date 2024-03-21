85d6de4094092524907a36b45f85c11ff7145322
Eric-Stewart <theericstewart@gmail.com>
2024-03-20 20:48:22 -0400

    for render site
    for render site

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const sqlite3 = require('sqlite3');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 
 const db = new sqlite3.Database('db/reinsurance.db');
